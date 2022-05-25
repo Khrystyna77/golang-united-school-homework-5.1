0bd2ec60f7fb0c0dc6cc0745c5f26a2377001350
Khrystyna2409 <xrustuna.di@gmail.com>
2022-05-25 13:44:39 +0300

    good

--- square.go
+++ square.go
@@ -9,14 +9,30 @@ type Square struct {
 	a     uint
 }
 
-func (receiver) End() Point {
-	// implement me
+func (receiver *Square) End() Point {
+	// implement m
+	v := receiver.start
+	var v2 uint = receiver.a
+	endx := v.x + int(v2)
+	endy := v.y + int(v2)
+	Output := Point{x: endx, y: endy}
+	//fmt.Println(Output)
+	return Output
+
 }
 
-func (receiver) Area() uint {
+func (receiver *Square) Area() uint {
 	// implement me
+	var uinta uint = receiver.a
+	uintAr := uinta * uinta
+	return uintAr
 }
 
-func (receiver) Perimeter() uint {
+func (receiver *Square) Perimeter() uint {
 	// implement me
+	var pers uint = receiver.a
+	persq := 2 * (pers + pers)
+	//fmt.Println(persq)
+	return persq
+
 }
